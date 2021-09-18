005b01ff66fbb519d1db71664debc94ac94085a4
Prathamesh More <prathameshmm02@gmail.com>
2021-09-18 09:48:07 +0530

    Fixed SettingsActivity.kt

--- app/src/main/java/com/inator/calculator/activities/SettingsActivity.kt
+++ app/src/main/java/com/inator/calculator/activities/SettingsActivity.kt
@@ -11,6 +11,7 @@ import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 import com.inator.calculator.R
 import com.inator.calculator.repository.Data
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -19,6 +20,7 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        setSupportActionBar(toolbar)
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
